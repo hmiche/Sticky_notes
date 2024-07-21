c97c5ace61c895f94d63ba82bb72669188840279
hmiche <mehdi.hmiche@gmail.com>
2024-07-21 01:48:25 +0100

    Adding HTML files

--- src/main/java/com/mvc/sticky_notes/controller/StickyNoteController.java
+++ src/main/java/com/mvc/sticky_notes/controller/StickyNoteController.java
@@ -3,13 +3,29 @@ package com.mvc.sticky_notes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StickyNoteController {
 
-    @GetMapping("/hello-world")
-    public String  Helloworld(){
+    @GetMapping("")
+    public String  home(){
         return "home";
+    }
+
+    @GetMapping("/my-notes")
+    public String  myNotes(){
+        return "layout/my-notes";
+    }
+
+    @GetMapping("/sticky-note/{id}")
+    public String  stickyNote(@PathVariable(name = "id") Integer id){
+        return "sticky-note";
+    }
+
+    @GetMapping("/edit-sticky-note/{id}")
+    public String  editStickyNote(@PathVariable(name = "id") Integer id){
+        return "edit-sticky-note";
     }
 }
